95a6c09f2c37d1939a59e6d7df1ce37ee4c8486e
Siddharth Bhat <siddu.druid@gmail.com>
2023-04-10 13:16:41 +0100

    wip, feat: get simplest version with goal unification working...

--- Matchgoal.lean
+++ Matchgoal.lean
@@ -31,16 +31,16 @@ scoped syntax (name := var) "#" noWs ident : unification_var
 -- scoped syntax unification_var : term -- allow unification variables to be used in terms.
 
 -- every unification variable is a term.
-open Lean Elab in 
+open Lean Elab in
 scoped elab (name := var2term) t:unification_var : term => do
-  let s : Syntax ← 
-    match t with 
+  let s : Syntax ←
+    match t with
     | `(unification_var| #$i:ident) => pure i
     | _ => throwUnsupportedSyntax
   elabTerm s (expectedType? := .none)
 
 
-scoped syntax (name := var2expr) (priority := low) 
+scoped syntax (name := var2expr) (priority := low)
   unification_var : unification_expr -- this needs a converter
 
 -- set this to have low priority because there are 2 ways in which vars can become exprs:
@@ -52,11 +52,14 @@ syntax hyps := sepBy(hyp_matcher, ";")
 
 structure MVar where
   id: MVarId
-  
+
 def MVar.toExpr (mvar : MVar) : Expr := Expr.mvar mvar.id
 def MVar.ofExpr: Expr → TacticM MVar
 | .mvar id => return .mk id
 | e => throwError "Internal tactic error: expected mvar, found expression {e}"
+
+open Lean Elab in
+def MVar.toSyntax (mvar : MVar) : TermElabM Syntax := exprToSyntax mvar.toExpr
 
 instance : Coe MVar Expr where
   coe := MVar.toExpr
@@ -72,12 +75,12 @@ structure PatternCtx where
   goalMatcher : Option MVar := .none
 deriving Inhabited
 
-def PatternCtx.restoreState (ctx: PatternCtx) : TacticM Unit := 
-  match ctx.tacticState with 
+def PatternCtx.restoreState (ctx: PatternCtx) : TacticM Unit :=
+  match ctx.tacticState with
   | .none => pure ()
   | .some state => MonadBacktrack.restoreState state
 
-def PatternCtx.saveState (ctx: PatternCtx) : TacticM PatternCtx := do 
+def PatternCtx.saveState (ctx: PatternCtx) : TacticM PatternCtx := do
   return { ctx with tacticState := (← MonadBacktrack.saveState) }
 
 -- TODO: think about order of monads?
@@ -87,39 +90,39 @@ def MatchGoalM α := TacticM (List α)
 def MatchGoalM.unwrap : MatchGoalM α → TacticM (List α) := id
 def MatchGoalM.wrap : TacticM (List α) → MatchGoalM α := id
 
-instance : Functor MatchGoalM where 
+instance : Functor MatchGoalM where
   map f mas := MatchGoalM.wrap do return (← mas).map f
 
 instance : Monad MatchGoalM where
   pure a := MatchGoalM.wrap (pure [a])
   bind mas a2mbs := MatchGoalM.wrap do
-    let mut xs := [] 
-    for a in (← mas.unwrap) do 
+    let mut xs := []
+    for a in (← mas.unwrap) do
       xs := xs.append (← a2mbs a)
     return xs
 
-instance : MonadBacktrack Tactic.SavedState MatchGoalM where 
+instance : MonadBacktrack Tactic.SavedState MatchGoalM where
   saveState := MatchGoalM.wrap <| do let s ← saveState; return [s]
-  restoreState state := MatchGoalM.wrap <| do restoreState state; return [()] 
+  restoreState state := MatchGoalM.wrap <| do restoreState state; return [()]
 
-instance : MonadLift TacticM MatchGoalM where 
+instance : MonadLift TacticM MatchGoalM where
   monadLift ma := MatchGoalM.wrap do return [← ma]
 
-instance : Alternative MatchGoalM where 
+instance : Alternative MatchGoalM where
   failure := MatchGoalM.wrap (return [])
   orElse  mas unit2mas := MatchGoalM.wrap do return (← mas) ++ (← unit2mas ())
 def runUnificationVar (ctx: PatternCtx) (e: Expr): TSyntax `unification_var → MatchGoalM PatternCtx
-| `(unification_var| #$i:ident) => do 
-  match ctx.mvars.find? i.getId with 
-  | .none => 
+| `(unification_var| #$i:ident) => do
+  match ctx.mvars.find? i.getId with
+  | .none =>
       ctx.restoreState
-      let mvar : MVar := ← MVar.ofExpr (← mkFreshExprMVar 
+      let mvar : MVar := ← MVar.ofExpr (← mkFreshExprMVar
           (type? := .none) (userName := i.getId) (kind := .natural))
-      guard (← isDefEq mvar.toExpr e) 
+      guard (← isDefEq mvar.toExpr e)
       return (← { ctx with mvars := ctx.mvars.insert i.getId mvar}.saveState)
-  | .some mvar => 
+  | .some mvar =>
       ctx.restoreState
-      guard (← isDefEq mvar.toExpr e) 
+      guard (← isDefEq mvar.toExpr e)
       ctx.saveState
 | _ => MatchGoalM.wrap <| throwUnsupportedSyntax
 
@@ -127,62 +130,90 @@ def runUnificationVar (ctx: PatternCtx) (e: Expr): TSyntax `unification_var → 
 -- instantiate all unification variables in the expression and create a real Lean term.
 -- Also update the state to recored new unification variables
 -- TODO: how do I look inside a term and find unification variables?
-open Lean Core Meta Elab Macro Tactic in 
+open Lean Core Meta Elab Macro Tactic in
 def runUnificationExpr (ctx: PatternCtx) (e: Expr) : TSyntax `unification_expr → MatchGoalM PatternCtx
 | `(unification_expr| $t:term ) => do
     let te ← Tactic.elabTerm (stx := t) (expectedType? := .none)
     guard (← isDefEq e te) -- mvar ~ te
     return ctx
-| `(unification_expr| $var:unification_var ) => runUnificationVar ctx e var 
-| _ => MatchGoalM.wrap <| throwUnsupportedSyntax 
+| `(unification_expr| $var:unification_var ) => runUnificationVar ctx e var
+| _ => MatchGoalM.wrap <| throwUnsupportedSyntax
 
-open Lean Core Meta Elab Macro Tactic in 
+open Lean Core Meta Elab Macro Tactic in
 def runHypMatcher (ctx: PatternCtx) : TSyntax `hyp_matcher → MatchGoalM PatternCtx
-| `(hyp_matcher| (#$i:ident : $e:unification_expr)) => do 
+| `(hyp_matcher| (#$i:ident : $e:unification_expr)) => do
     ctx.restoreState
-    (← getMainDecl).lctx.decls.foldl (init := pure ctx) (fun mctx ldecl? => do 
+    (← getMainDecl).lctx.decls.foldl (init := pure ctx) (fun mctx ldecl? => do
       ctx.restoreState
-      match ldecl? with 
-      | .none => return ctx 
+      match ldecl? with
+      | .none => return ctx
       | .some ldecl => Alternative.orElse mctx (fun () => do
         let ldeclType : Expr ← inferType ldecl.toExpr
         let newctx ← runUnificationExpr ctx ldeclType e
         newctx.saveState -- TODO: this should be un-necessary
       ))
-| _ => MatchGoalM.wrap <| throwUnsupportedSyntax 
-  
+| _ => MatchGoalM.wrap <| throwUnsupportedSyntax
 
-  
+
+-- Substitute holes in the Syntax given by `unification_var` with the values in ctx
+open Lean Elab Macro Tactic in
+partial def substitute (ctx: PatternCtx) (s : Syntax) : MatchGoalM Syntax :=
+  match s with
+  | `(term| #$i:ident) => do substitute ctx (← monadLift (m := TacticM) <| `(unification_var| #$i))
+  | `(unification_var| #$i:ident) => do
+     -- Here we use the nasty trick of converting an `MVar` into a `Syntax` object.
+     match ctx.mvars.find? i.getId with
+     | .none => do
+        logErrorAt s <| MessageData.tagged `Tactic.Matchgoal <| m!"Matchgoal variable {s} has not been unified. This is an error."
+        return s
+     | .some mvar => mvar.toSyntax
+  | _ =>
+    match s with
+    | Syntax.node info kind args => do return Syntax.node info kind (← args.mapM (substitute ctx))
+    | Syntax.missing | Syntax.atom .. | Syntax.ident .. => return s
+
+
+
 
 -- match goal tactic
 -- TODO: why does 'local syntax' not work?
--- local syntax (name := matchgoal) 
-scoped syntax (name := matchgoal) 
+-- local syntax (name := matchgoal)
+scoped syntax (name := matchgoal)
   "matchgoal"
   (hyps)?
   "⊢" (( unification_expr)? <|>  "_") "=>" tactic : tactic
 
-open Lean Meta Elab Tactic in 
+open Lean Meta Elab Tactic in
 #check evalSubst
-#check Syntax 
+#check Syntax
 
-open Lean Core Meta Elab Macro Tactic in 
+open Lean Core Meta Elab Macro Tactic in
 @[tactic MatchGoal.matchgoal]
-def evalMatchgoal : Tactic := fun stx => -- (stx -> TacticM Unit) 
-  match stx with 
-  | `(tactic| matchgoal 
+def evalMatchgoal : Tactic := fun stx => -- (stx -> TacticM Unit)
+  match stx with
+  | `(tactic| matchgoal
         $[ $[ $hs? ];* ]?
         ⊢ _ => $t ) => do
-        return () 
-      
-  | `(tactic| matchgoal 
-        $[ $[ $hs? ];* ]?
-        ⊢ $[ $g?:unification_expr ]? => $t ) => do 
+        return ()
+
+  | `(tactic| matchgoal
+      $[ $[ $hs? ];* ]?
+      ⊢ $[ $g?:unification_expr ]? => $t ) => do
         IO.println g?
-        return () 
-  -- let PatternCtx ←  do 
-      -- if let .some g := g? then runGoalMatcher (← getMainDecl).type g -- first bind goal.
-      -- if let .some hs := hs? then hs.forM runHypMatcher
+        let ts ← MatchGoalM.unwrap do
+          let mut ctx : PatternCtx := default
+          if let .some g := g? then
+             ctx ← runUnificationExpr ctx (← getMainDecl).type g -- bind the goal
+          substitute ctx t
+        -- we have many tactics to run
+        if ts.length != 1
+        then logErrorAt t <| MessageData.tagged `Tactic.Matchgoal m!"expected exactly one output state, found {ts}"
+        else
+          for t in ts do
+          match ← tryTactic? (evalTactic t) with
+          | .some () => return -- we succeeded with the invocation
+          | none => continue -- we failed, so we try the next invocation
+        return ()
   | _ => throwUnsupportedSyntax
 
 end MatchGoal
